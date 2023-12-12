9691862e33e601cb628c7966abc0e3914c813e69
p s shajahan <poolasyedshajahan123@gmail.com>
2023-12-12 22:06:23 +0530

    second commit

--- frontend/src/components/home/BooksSingleCard.jsx
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
+import { BiShow, BiUserCircle } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import {useState} from 'react'
+import BookModel from "./BookModel";
 
 const BooksSingleCard = ({ book }) => {
+  const [showModel, setShowModel] = useState(false)
   return (
     <div
       key={book._id}
@@ -24,6 +27,9 @@ const BooksSingleCard = ({ book }) => {
         <h2 className="my-1">{book.author}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow 
+        className="text-3xl text-blue-800 hover:text-black cursor-pointer" 
+        onClick={() => setShowModel(true)}/>
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-green-800 text-2xl hover:text-black" />
         </Link>
@@ -34,6 +40,9 @@ const BooksSingleCard = ({ book }) => {
           <MdOutlineDelete className="text-red-600 text-2xl hover:text-black" />
         </Link>
       </div>
+      {
+        showModel && <BookModel book={book} onClose={() => setShowModel(false)} />
+      }
     </div>
   );
 };
